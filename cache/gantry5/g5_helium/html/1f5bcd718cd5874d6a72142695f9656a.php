9934a4c237a26bf891c7342a342a7c299fdb55c9
coptercaps <emil@Emils-MacBook-Air.local>
2017-04-18 19:54:19 +0200

    update

---
+++ cache/gantry5/g5_helium/html/1f5bcd718cd5874d6a72142695f9656a.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58f652b75f7f59.57914009',
+    'content' => '<div class="g-content g-particle">
+                                <div id="g-mobilemenu-container" data-g-menu-breakpoint="48rem"></div>
+            
+    </div>'
+];
