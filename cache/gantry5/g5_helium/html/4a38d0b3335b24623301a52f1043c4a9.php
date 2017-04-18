afee934cc9c3305572ff1117648847fb15d0baf7
coptercaps <emil@Emils-MacBook-Air.local>
2017-04-18 22:03:28 +0200

    Update

---
+++ cache/gantry5/g5_helium/html/4a38d0b3335b24623301a52f1043c4a9.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58f6702a4db2e4.91191625',
+    'content' => '<div class="g-content g-particle">
+                                <div id="g-mobilemenu-container" data-g-menu-breakpoint="48rem"></div>
+            
+    </div>'
+];
---
+++ cache/gantry5/g5_helium/html/ed0dbd0ed742102d0ea2a6481f2b788c.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58f66f131a0fc2.32318669',
+    'content' => '<div class="g-content g-particle">
+                                    <div class="g-social ">
+                    <a target="_blank" href="http://www.facebook.com/RocketTheme" title="" aria-label="">
+                <span class="fa fa-facebook fa-fw"></span>                <span class="g-social-text"></span>            </a>
+                    <a target="_blank" href="" title="" aria-label="">
+                <span class="fa fa-linkedin"></span>                <span class="g-social-text"></span>            </a>
+            </div>
+            
+    </div>'
+];
