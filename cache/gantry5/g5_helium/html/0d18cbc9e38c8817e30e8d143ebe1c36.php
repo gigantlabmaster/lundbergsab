a6bfa73d568605f4840fdf98ee5ff548947ee255
coptercaps <emil@Emils-MacBook-Air.local>
2017-04-18 19:51:45 +0200

    update

---
+++ cache/gantry5/g5_helium/html/0d18cbc9e38c8817e30e8d143ebe1c36.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58f652239b32d0.50607896',
+    'content' => '<div class="g-content g-particle">
+                                    <div class="fp-intro">
+    <h2 class="g-title">Build your beautiful theme, the way you want it, with Helium</h2>
+    <p>Craft memorable, emotive experiences with our Gantry 5 framework.</p>
+    <div class="ipad-mockup">
+        <img src="/user/themes/g5_helium/images/intro/mockup.png" alt="" />
+    </div>
+</div>
+            
+    </div>'
+];
---
+++ cache/gantry5/g5_helium/html/68ac84c82bce70b992ec143e537e4344.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58f6522644b0f3.00756400',
+    'content' => '<div class="g-content g-particle">
+                                <div id="g-mobilemenu-container" data-g-menu-breakpoint="48rem"></div>
+            
+    </div>'
+];
